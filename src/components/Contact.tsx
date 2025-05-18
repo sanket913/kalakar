c77aad6f41127b6880813ec6aa341940250e0540
SANKET PRAJAPATI <87607558+sanket913@users.noreply.github.com>
2025-05-18 17:23:29 +0530

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -154,7 +154,7 @@ const Contact: React.FC = () => {
                   </div>
                   <div>
                     <h4 className="text-lg font-semibold">Email</h4>
-                    <p className="text-gray-600">info@kalakarartacademy.com</p>
+                    <p className="text-gray-600">artbykalakaar@gmail.com</p>
                   </div>
                 </div>
                 
